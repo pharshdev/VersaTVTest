89d79562c3ed9f46b405d858f6f1a126dcec394a
Harsh <harshpatil119@gmail.com>
2020-11-10 12:13:02 +0530

    Initial Commit

---
+++ VersaTVTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VersaTVTest
+//
+//  Created by Harsh on 10/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VersaTVTest/VersaTVTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VersaTVTestApp.swift
+//  VersaTVTest
+//
+//  Created by Harsh on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct VersaTVTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
